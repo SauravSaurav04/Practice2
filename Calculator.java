a0267663664fab93b7e929219806dbe99d7bbd85
SauravSaurav04 <sauravsaurav4444@gmail.com>
2024-03-11 17:41:15 +0530

    'change1'

---
+++ Calculator.java
@@ -0,0 +1,22 @@
+public class Calculator {
+    
+    static int add(int a, int b){
+        return 0;
+    }
+    
+    static int sub(int a, int b){
+        return 0;
+    }
+    
+    static int mul(int a, int b){
+        return 0;
+    }
+
+    static int div(int a, int b){
+        return 0;
+    }
+
+    public static void main(String []args){
+        System.out.println("Hello World");
+    }
+}
